f275046c1724b521bd0f44578f2b1509ba1c4932
Piosuke <Piosuke@Piosukes-MacBook-Pro.local>
2018-06-21 20:58:03 -0400

    finished doggo state

--- public/actions/dogs-actions.js
+++ public/actions/dogs-actions.js
@@ -1,1 +1,15 @@
 // put stuff here
+let getDogs = ()=>{
+    store.dispatch({
+        type: "DOG_SEARCH_INITIATED"
+    })
+    $.ajax({
+        method: "GET",
+        url: "https://dog.ceo/api/breeds/image/random"
+    }).then((dogData)=> {
+        store.dispatch({
+            type: "DOG_FOUND",
+            dog: dogData.message
+        })
+    })
+}
--- public/reducers/dogs-reducer.js
+++ public/reducers/dogs-reducer.js
@@ -1,8 +1,19 @@
 let dogsDefaultState = {
-	// put stuff here
+	dogPicture: "",
+	isLoading: false
 }
 
 const dogsReducer = (state = dogsDefaultState, action) => {
-	// put stuff here too
+	
+	let stateCopy = {...state};
+
+	if(action.type === "DOG_SEARCH_INITIATED"){
+		stateCopy.isLoading = true;
+	}else if(action.type === "DOG_FOUND"){
+		let { dog } = action;
+		stateCopy.dogPicture = dog;
+		stateCopy.isLoading = false;
+		return stateCopy;
+	}
 	return state;
 }
